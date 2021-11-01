18569414795e30420d086ac6a0a7574f9941612a
Yann Figueiredo <yann.fabricio@hotmail.com>
2021-10-31 23:14:47 -0300

    Finalizdaas parcilamente funções do carrosel

--- js/carrosel.js
+++ js/carrosel.js
@@ -1,31 +1,68 @@
 function Carrosel(config){
     this.container = document.querySelector(config.container);
+    this.tam_container = config.tam_container;
     this.itens = document.querySelectorAll(config.itens);
     this.btnAnterior = document.querySelector(config.btn_anterior);
     this.btnProximo = document.querySelector(config.btn_proximo);
 
     var _this = this;
     var slideAtual = 0;
+    var posicaoAtual = 0;
+    var deslocamento = _this.tam_container/calcularDeslocamento();
+
+    _this.btnAnterior.addEventListener('click', function(){
+        voltarSlide();
+
+        atualizarSlideAtual();
+    });
 
     _this.btnProximo.addEventListener('click', function(){
-        console.log('clicado');
         avancarSlide();
+
+        atualizarSlideAtual();
     });
 
     function calcularDeslocamento(){
-        console.log(Math.abs(1250/_this.container.innerWidth));
-        return Math.abs(1250/_this.container.innerWidth);
+        return Math.abs(_this.tam_container/_this.container.clientWidth);
     }
 
     function avancarSlide(){
         for(var i = 0; i < calcularDeslocamento(); i++){
-            _this.itens.style.transform = 'translateX(-'+ 1250/calcularDeslocamento() +')';
+            console.log('slide atual: ', slideAtual);
+            console.log('slide posterior: ', slideAtual+1);
+            posicaoAtual = Math.abs(posicaoAtual) >= checarDeslocamento() ? 0 : posicaoAtual - deslocamento;
+            console.log('posicao atual: ', posicaoAtual);
+
+            _this.itens[slideAtual].style.transform = 'translateX('+ posicaoAtual +'px)';
+            _this.itens[slideAtual+1 >= _this.itens.length ? 0 : slideAtual+1].style.transform = 'translateX('+ posicaoAtual +'px)';
         }
 
         slideAtual += 1;
     }
 
     function voltarSlide(){
+        for(var i = 0; i < calcularDeslocamento(); i++){
+            console.log('slide atual: ', slideAtual);
+            console.log('slide posterior: ', slideAtual-1);
+            posicaoAtual = posicaoAtual == 0 ? -1*checarDeslocamento() : posicaoAtual + deslocamento;
+            console.log('posicao atual: ', posicaoAtual);
 
+            _this.itens[slideAtual].style.transform = 'translateX('+ posicaoAtual +'px)';
+            _this.itens[slideAtual-1 < 0 ? _this.itens.length-1  : slideAtual-1].style.transform = 'translateX('+ posicaoAtual +'px)';
+        }
+
+        slideAtual -= 1;
+    }
+
+    function atualizarSlideAtual(){
+        if(slideAtual >= _this.itens.length){
+            slideAtual = 0;
+        }else if(slideAtual < 0){
+            slideAtual = _this.itens.length - 1;
+        }
+    }
+
+    function checarDeslocamento(){
+        return (_this.itens.length-1)*(calcularDeslocamento()*_this.tam_container);
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 (function(){
     carrosel_consoles = new Carrosel({
         container: '.carrosel-consoles',
+        tam_container: 1250,
         itens: '.carrosel-consoles img',
         btn_anterior: '.carrosel-consoles .btn-anterior',
         btn_proximo: '.carrosel-consoles .btn-proximo'
